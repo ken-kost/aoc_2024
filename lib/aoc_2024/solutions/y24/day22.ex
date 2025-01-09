827bfef2cefe2eca2b04890738f02555ca0045aa
ken-kost <kenneth.kostresevic@gmail.com>
2025-01-09 18:14:07 +0100

    Solve day 22 part 2

--- lib/aoc_2024/solutions/y24/day22.ex
+++ lib/aoc_2024/solutions/y24/day22.ex
@@ -19,13 +19,41 @@ defmodule Aoc2024.Solutions.Y24.Day22 do
     |> Enum.sum()
   end
 
+  def part_two(problem) do
+    problem
+    |> Input.stream!(trim: true)
+    |> Enum.reduce(%{}, fn secret, sales ->
+      secret
+      |> String.to_integer()
+      |> Stream.iterate(&next_secret/1)
+      |> Stream.map(&rem(&1, 10))
+      |> Stream.chunk_every(2, 1)
+      |> Stream.map(fn [a, b] -> {b - a, b} end)
+      |> Enum.take(2000)
+      |> Enum.chunk_every(4, 1, :discard)
+      |> Enum.flat_map(fn seq ->
+        [_, _, _, {_, price}] = seq
+        # Optimization: compress the sequence.
+        seq =
+          Enum.map(seq, &elem(&1, 0))
+          |> Enum.reduce(0, fn item, acc ->
+            acc * 20 + (item + 10)
+          end)
+
+        [{seq, price}]
+      end)
+      |> Enum.uniq_by(&elem(&1, 0))
+      |> Enum.reduce(sales, fn {seq, price}, sales ->
+        Map.update(sales, seq, price, &(&1 + price))
+      end)
+    end)
+    |> Enum.max_by(fn {_, total_price} -> total_price end)
+    |> elem(1)
+  end
+
   defp next_secret(secret, mask \\ 16_777_215) do
     secret = band(bxor(secret <<< 6, secret), mask)
     secret = band(bxor(secret >>> 5, secret), mask)
     band(bxor(secret <<< 11, secret), mask)
   end
-
-  # def part_two(problem) do
-  #   problem
-  # end
 end
--- test/2024/day22_test.exs
+++ test/2024/day22_test.exs
@@ -49,17 +49,16 @@ defmodule Aoc2024.Solutions.Y24.Day22Test do
   #   assert {:ok, @part_one_solution} == AoC.run(2024, 22, :part_one)
   # end
 
-  # test "part two example" do
-  #   input = ~S"""
-  #   This is an
-  #   example input.
-  #   replace with
-  #   an example from
-  #   the AoC website.
-  #   """
-  #
-  #   assert CHANGE_ME == solve(input, :part_two)
-  # end
+  test "part two example" do
+    input = ~S"""
+    1
+    10
+    100
+    2024
+    """
+
+    assert 23 == solve(input, :part_two)
+  end
 
   # You may also implement a test to validate the part two to ensure that you
   # did not broke your shared modules when implementing another problem.
