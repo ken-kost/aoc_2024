dc669623b22ff76dee534613d3f3289bfe839a6e
ken-kost <kenneth.kostresevic@gmail.com>
2025-01-10 14:16:00 +0100

    D01/2015 - solution

--- lib/aoc_2024/solutions/y15/day01.ex
+++ lib/aoc_2024/solutions/y15/day01.ex
@@ -2,33 +2,21 @@ defmodule Aoc2024.Solutions.Y15.Day01 do
   alias AoC.Input
 
   def parse(input, _part) do
-    # This function will receive the input path or an %AoC.Input.TestInput{}
-    # struct. To support the test you may read both types of input with either:
-    #
-    # * Input.stream!(input), equivalent to File.stream!/1
-    # * Input.stream!(input, trim: true), equivalent to File.stream!/2
-    # * Input.read!(input), equivalent to File.read!/1
-    #
-    # The role of your parse/2 function is to return a "problem" for the solve/2
-    # function.
-    #
-    # For instance:
-    #
-    # input
-    # |> Input.stream!()
-    # |> Enum.map!(&my_parse_line_function/1)
-
-    Input.read!(input)
+    input |> Input.read!() |> String.to_charlist()
   end
 
   def part_one(problem) do
-    # This function receives the problem returned by parse/2 and must return
-    # today's problem solution for part one.
-
-    problem
+    Enum.reduce(problem, 0, fn
+      40, acc -> acc + 1
+      41, acc -> acc - 1
+    end)
   end
 
-  # def part_two(problem) do
-  #   problem
-  # end
+  def part_two(problem) do
+    Enum.reduce_while(problem, {0, 0}, fn
+      _e, {-1, index} -> {:halt, index}
+      40, {floor, index} -> {:cont, {floor + 1, index + 1}}
+      41, {floor, index} -> {:cont, {floor - 1, index + 1}}
+    end)
+  end
 end
